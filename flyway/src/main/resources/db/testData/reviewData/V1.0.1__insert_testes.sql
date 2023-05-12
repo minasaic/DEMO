eb82daf7f28c6f9bdc9688fa2940d472ae25768e
kaihatsu-chikara-morigami <kaihatsu.chikara.morigami@gmail.com>
2023-05-12 13:44:27 +0900

    森上　画像追加

--- flyway/src/main/resources/db/testData/reviewData/V1.0.1__insert_testes.sql
+++ flyway/src/main/resources/db/testData/reviewData/V1.0.1__insert_testes.sql
@@ -12,17 +12,46 @@ INSERT INTO user(id,name,password,profile_picture)VALUES
 (11,'古郡','pass','images.png');
 
 
-INSERT INTO posts(id,userid,image,caption,likes)VALUES
-(1,1,'images1.jpeg','プロのエンジニアだぞ！',1),
-(2,3,'KJ_20201215_02_01.jpg','瀬戸内海楽しい！！！〜〜',2),
-(3,4,'atami2.jpeg','熱海行ってきた！',3),
-(4,5,'jkl.jpeg','私めちゃくちゃ可愛い！',4),
-(5,6,'DSC06150.jpg','世界最強の！',5),
-(6,7,'lkjhgfd.jpeg','美人！！',6),
-(7,8,'1100x770_ea1e9d427fb5664c32c517a73e421e58_6caa40561048704c2c3dd9eafcd154384ef8fca6.jpg','旅行いいな！',7),
-(8,9,'kafe2.jpeg','仕事頑張ろう！',8),
-(9,10,'9d09f489ee0edc4845f30ab5ccf1283c.png','焼肉食べたい！！',9),
-(10,11,'article_img_19121701_25.jpg','刺身食べたい！！',10);
+INSERT INTO posts(userid,image,caption,likes)VALUES
+(1,'images1.jpeg','プロのエンジニアだぞ！',0),
+(3,'KJ_20201215_02_01.jpg','瀬戸内海楽しい！！！〜〜',0),
+(4,'atami2.jpeg','熱海行ってきた！',0),
+(5,'jkl.jpeg','私めちゃくちゃ可愛い！',0),
+(6,'DSC06150.jpg','世界最強の！',0),
+(7,'lkjhgfd.jpeg','美人！！',0),
+(8,'1100x770_ea1e9d427fb5664c32c517a73e421e58_6caa40561048704c2c3dd9eafcd154384ef8fca6.jpg','旅行いいな！',0),
+(9,'kafe2.jpeg','仕事頑張ろう！',0),
+(10,'9d09f489ee0edc4845f30ab5ccf1283c.png','焼肉食べたい！！',0),
+(11,'article_img_19121701_25.jpg','刺身食べたい！！',0),
+(1,'Autumn-trees-lake-house-park-Japan_1920x1200.jpg','ああああ',0),
+(1,'Night-sky-stars-starry_2560x1440.jpg','ああああああああああああああああああああ',0),
+(1,'Philadelphia-Delaware-river-Benjamin-Franklin-Bridge-USA-night-lights_1920x1200.jpg','ああああああ',0),
+(1,'Blonde-girl-autumn-leaves_3840x2160.jpg','嗚呼ああ',0),
+(1,'New-York-old-town-street-colorful-lights-night-USA_1920x1200.jpg','あああああ',0),
+(1,'Whip-Snake_1920x1440.jpg','あああああああ',0),
+(2,'Turtle-front-view-ground_1920x1200.jpg','あああああああ',0),
+(2,'Three-tulips-flowers-cloth-background_1920x1200.jpg','ああああああ',0),
+(2,'Tomatoes-vegetables-juice-glass-cup_3840x2160.jpg','ああああああ',0),
+(2,'Two-guns-weapon_1920x1440.jpg','ああああああ',0),
+(2,'Pink-rose-and-bud-green-background_3840x2160.jpg','あああ',0),
+(2,'Beach-sea-sunset-city-skyscrapers-night_1920x1080.jpg','あああ',0),
+(2,'Asian-girl-heavy-rain-umbrella_1920x1080.jpg','あああ',0),
+(3,'アーニャイエッサー.jpg','あああ',0),
+(3,'2cde853ca5f0138eb9498cdb170a3bed.png','あああ',0),
+(3,'05a60a4029542f809a7be0124007fe73.png','あああ',0),
+(3,'Netherlands-river-windmill-grass-beautiful-scenery_1920x1080.jpg','あああ',0),
+(3,'3d481d0cd307567edf359b5f70f41730.png','あああ',0),
+(4,'f55400b9140273fcbab4fead55147a5b.png','あああ',0),
+(4,'ee595fe6b57f698ea1659a36872d3fde.png','ああああ',0),
+(4,'42062cbeca16839af3efe8a7d61ceb27a30f758b.png','あああ',0),
+(4,'91c067049a22890550a4e9823c046282.png','あああああ',0),
+(4,'4986a33f760c4fd1f13416d1d0b131a9.png','ああああ',0),
+(4,'Blonde-girl-autumn-leaves_3840x2160.jpg','ああああ',0),
+(5,'a6e90225ab9058e6c6268441a48636fe.png','ああああ',0),
+(5,'FTSz33FaIAE5Eis.jpeg','ああああ',0),
+(5,'d14276-158-1e9f119437078c220e26-0.jpg','ああああ',0),
+(5,'img_f944e7d3040fd983ad5fb017054e18c01924172.jpg','ああああ',0);
+
 
 
 INSERT INTO follows(id,followerid,followingid)VALUES
